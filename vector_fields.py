7abc0514d7127837ddb6fa8af3d7188ef0c913e5
thwh <junky.piler@gmail.com>
2023-05-02 11:34:10 -0700

    elliptical firing path, with resting state hole

--- vector_fields.py
+++ vector_fields.py
@@ -1,13 +1,11 @@
 import itertools as it
 import math
+from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from scipy.interpolate import NearestNDInterpolator
-
-from matplotlib.patches import Ellipse, Annulus
-from matplotlib.path import Path
-from matplotlib.patches import PathPatch
 
 
 def PlotShow(plot=None):
@@ -22,9 +20,31 @@ def PlotShow(plot=None):
     plt.close("all")
 
 
-def CircleVertices(center, radius, size):
-    circ = plt.Circle(center, radius, edgecolor="b", facecolor="None")
-    return circ.get_path().vertices * size
+def GetVertices(polygon):
+    return polygon.get_path().vertices
+
+
+def CircleVertices(center, radius):
+    return GetVertices(patches.Circle(center, radius))
+
+
+def EllipseVertices(center, width, height, angle):
+    return (
+        GetVertices(
+            # these parameters aren't doing anything when I get the path:
+            patches.Ellipse(center, width, height, angle=angle)
+        )
+        * (width, height)
+        + center
+    )
+
+
+def RectangleVertices(center, width, height, angle):
+    return (
+        GetVertices(patches.Rectangle(center, width, height, angle=angle))
+        * (width, height)
+        + center
+    )
 
 
 def NN():
@@ -45,14 +65,17 @@ def NN():
     plt.legend()
     plt.colorbar()
     plt.axis("equal")
-    # plt.show()
     PlotShow()
 
 
-def Radians(point, vertex):
+def Slope(point, vertex):
     px, py = point
     vx, vy = vertex
-    return math.atan2((py - vy), (px - vx))
+    return (py - vy), (px - vx)
+
+
+def Radians(point, vertex):
+    return math.atan2(Slope(point, vertex))
 
 
 def NearestVertex(point, vertices):
@@ -60,6 +83,7 @@ def NearestVertex(point, vertices):
         {
             "coord": v,
             "radians": Radians(point, v),
+            # "slope": Slope(point, v),
             "distance": np.linalg.norm(point - v),
         }
         for v in vertices
@@ -67,38 +91,48 @@ def NearestVertex(point, vertices):
     return min(ls, key=lambda x: x["distance"])
 
 
-def ImpressField(field, vertices):
+def ImpressField(field, vertices, strength):
     for i, (point, (distance, radians)) in enumerate(field):
         nearest = NearestVertex(point, vertices)
         field[i] = (
-            point, (
-                nearest["distance"] - distance,
-                nearest["radians"] - radians
-            )
+            point,
+            (
+                mean(((strength * nearest["distance"]), distance)),
+                (strength * nearest["radians"]) - radians,
+            ),
         )
 
 
 def PlotField(field):
-
     X = field[:, [0], [0]]
     Y = field[:, [0], [1]]
     distances = field[:, [1], [0]]
 
     plt.scatter(X, Y, c=distances)
+    plt.colorbar(label="firing path resistance")
+    plt.axis("equal")
     PlotShow()
 
 
 if __name__ == "__main__":
-    size = 100
-    vertices = CircleVertices((0, 0), 0.7, size)
+    size = 10
+    Z_FIELDS = 2
 
-    x, y = vertices[:, 0], vertices[:, 1]
-    X = np.linspace(min(x) - 0.5, max(x) + 0.5, num=size)
-    Y = np.linspace(min(y) - 0.5, max(y) + 0.5, num=size)
+    # vertices = CircleVertices((0, 0), 0.5)
+    # vertices = RectangleVertices((0., 0.), 0.9, 0.3, 30)
+    vertices = EllipseVertices((0.3, 0.3), 0.9, 0.45, 30)
+    hole = np.array([[-1.3, -1.3]])
+
+    # x, y = vertices[:, 0], vertices[:, 1]
+    # X = np.linspace(min(x) - 0.5, max(x) + 0.5, num=size)
+    # Y = np.linspace(min(y) - 0.5, max(y) + 0.5, num=size)
+    X = np.linspace(-1.5, 1.5, num=size)
+    Y = np.linspace(-1.5, 1.5, num=size)
 
     coords = np.array(list(it.product(X, Y)))
-    Z = np.zeros(coords.shape)
+    Z = np.zeros((len(coords), Z_FIELDS))
     field = np.array(list(zip(coords, Z)))
 
-    ImpressField(field, vertices)
+    ImpressField(field, hole, strength=0.5)
+    ImpressField(field, vertices, strength=1)
     PlotField(field)
