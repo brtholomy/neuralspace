7ef104d98680f1cff1e23c0916b9939e1ac219a9
bth <gh@hautogdoad.com>
2023-05-21 09:13:50 -0700

    impress multiple shapes onto the resistance field

--- field_continuous.py
+++ field_continuous.py
@@ -1,36 +1,50 @@
 import math
 
 import sympy as sp
+from sympy.geometry.entity import GeometryEntity
 
 
 class Field:
-    def __init__(self, ge, dist_func=None):
-        self.ge = ge
-        self.dist_func = self.Sigmoid if not dist_func else dist_func
+    def __init__(self, ge: GeometryEntity, dist_func=None):
+        dist_func = self.Sigmoid if not dist_func else dist_func
+        self._shapes = [(ge, dist_func)]
+
+    def AddShape(self, ge: GeometryEntity, dist_func=None):
+        dist_func = self.Sigmoid if not dist_func else dist_func
+        self._shapes.append((ge, dist_func))
 
     def Sigmoid(self, x):
-        # A sigmoid curve displaced down and normalized to (0,1)
+        # A sigmoid curve displaced down and normalized to (0,1).
+        # Note we ensure the incoming value is >= 0
+        x = abs(x)
         return ((1 / (1 + math.exp(-x))) - 0.5) * 2
 
     def CosHyperbola(self, x):
         # The cosh hyperbola is gentler than x^2 or e^x
-        return math.cosh(x) - 1
+        return min(1, math.cosh(x) - 1)
 
     def EulerHyperbola(self, x):
-        # The most dramatic curve:
-        return math.exp(x) - 1
+        # The most dramatic curve, maxing out at 1:
+        return min(1, math.exp(x) - 1)
 
     def _DistanceMetric(self, p1, p2):
         return abs(math.dist(p1, p2))
 
-    def _AbsDistance(self, p):
+    def _AbsDistance(self, ge, p):
         # draws a line from GeometryEntity center to the given point, and asks
         # for the intersections (the line is infinitely long, so it will
         # intersect twice for any ellipse if it's not an exact tangent).
-        intersections = self.ge.intersection(sp.Line(self.ge.center, p))
+        intersections = ge.intersection(sp.Line(ge.center, p))
+
         # find the intersection with the minimum distance.
-        # note the use of .evalf() to convert the sympy rationals to float:
+        # NOTE: the use of .evalf() to convert the sympy rationals to float,
+        # which is what slows everything down. Unfortunately there doesn't seem
+        # to be a way to prevent the list of Point() returned by intersection()
+        # from creating rational internal representations.
         return min(self._DistanceMetric(i.evalf(), p) for i in intersections)
 
     def ResistanceAt(self, point):
-        return self.dist_func(self._AbsDistance(point))
+        res = 0
+        for ge, dfunc in self._shapes:
+            res += dfunc(self._AbsDistance(ge, point))
+        return res
--- particle.py
+++ particle.py
@@ -35,9 +35,18 @@ def LeastResistantJump(p: Particle, f: Field):
     # sweep left to right with a specific granularity
     for i in np.linspace(-1, 1, p.jspec.granularity):
         radians = p.inertia.radians - (p.jspec.arc * i)
+
+        # FIXME: this arg is an attempt to speed up. sympy can be horribly
+        # slow, because of the conversion from real to rational numbers.
+        # https://github.com/sympy/sympy/issues/6716#issuecomment-37007389
+        # however, if we instantiate the Point here as a float, it will just get
+        # converted to a rational during the intersection() call later, which
+        # seems to make it even worse.
+        # evaluate=False
         offset = sp.Point(
             # get the offset coord by multiplying the radius * unit circle value:
-            [p.jspec.radius * i for i in (math.cos(radians), math.sin(radians))]
+            [p.jspec.radius * i for i in (math.cos(radians),
+                                          math.sin(radians))]
         )
         # sp.Point allows this convenient expression:
         pos = p.position + offset
@@ -99,11 +108,18 @@ def PlotField(f: Field, xymax: tuple, granularity: int):
 
 
 if __name__ == "__main__":
-    c = sp.Point(0, 0)
+    center = sp.Point(0, 0)
     vradius = 2
     hradius = 4
-    ge = sp.Ellipse(c, hradius, vradius)
-    f = Field(ge)
+    firing = sp.Ellipse(center, hradius, vradius)
+
+    center = sp.Point(-4, 0)
+    vradius = 0.2
+    hradius = 0.2
+    resting = sp.Ellipse(center, hradius, vradius)
+
+    f = Field(firing)
+    f.AddShape(resting, f.EulerHyperbola)
 
     m = Inertia(1, math.pi / 2)
     jspec = JumpSpec(0.25, math.pi / 4, 5)
@@ -120,4 +136,4 @@ if __name__ == "__main__":
     PlotPositions(positions)
 
     # this is expensive:
-    PlotField(f, (5, 5), 50)
+    # PlotField(f, (5, 5), 100)
